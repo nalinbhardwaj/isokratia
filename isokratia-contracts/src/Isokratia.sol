a0384f55c54d7ac6a6ab4dd626b2144af97e862a
Supernovahs.eth <91280922+supernovahs@users.noreply.github.com>
2022-09-24 01:12:01 +0530

    Feat:Gas Optimizations

--- isokratia-contracts/src/Isokratia.sol
+++ isokratia-contracts/src/Isokratia.sol
@@ -10,6 +10,13 @@ contract Isokratia is Verifier {
     event ProposalCreated(address creator, uint256 proposalId, uint256 endBlock);
     event AggregateCreated(uint256 proposalId, string option, uint256 voteCount);
 
+    ///////////////////// Errors////////////////////////
+
+    error ALREADYEXISTS();
+    error BAD_PROOF();
+    error EXPIRED();
+    error BADCOMMITMENT();
+
     struct Proposal {
         uint256 id;
         uint256 endBlock;
@@ -27,77 +34,93 @@ contract Isokratia is Verifier {
     function createProposal(uint256 proposalId,
         uint256 endBlock,
         uint256 eligibleRoot,
-        uint64[k1][] memory options,
-        string[] memory optionText) public {
-        require(proposals[proposalId].id == 0, "Proposal already exists");
+        uint64[k1][] calldata options,
+        string[] calldata optionText) public {
+        if (proposals[proposalId].id !=0) {
+            revert ALREADYEXISTS();
+        }
         proposals[proposalId].id = proposalId;
         proposals[proposalId].endBlock = endBlock;
         proposals[proposalId].eligibleRoot = eligibleRoot;
-        for (uint256 i = 0; i < options.length; i++) {
+        uint length = options.length;
+        uint256 i;
+        for (; i <length; ) {
             proposals[proposalId].options[optionText[i]] = options[i];
+            unchecked{
+                ++i;
+            }
         }
         emit ProposalCreated(msg.sender, proposalId, endBlock);
     }
 
     function postAggregation(uint256 proposalId,
-        string memory option,
+        string calldata option,
         uint256 voteCount,
         uint256 voterRoot,
-        uint256[2] memory _a,
-        uint256[2][2] memory _b,
-        uint256[2] memory _c,
-        uint256[1] memory _input) public {
-        require(block.number <= proposals[proposalId].endBlock, "Proposal has expired");
-        require(verifyProof(_a, _b, _c, _input), "Bad proof");
+        uint256[2] calldata _a,
+        uint256[2][2] calldata _b,
+        uint256[2] calldata _c,
+        uint256[1] calldata _input) public {
+        if (block.number > proposals[proposalId].endBlock){
+            revert EXPIRED();
+        }
+        if(!verifyProof(_a, _b, _c, _input)){
+            revert BAD_PROOF();
+        }
 
         uint256[3 + k1 + 6 * 2 * k2 + 3 * 2 * 2 * k2] memory commitmentInputs;
         commitmentInputs[0] = voteCount;
         commitmentInputs[1] = proposals[proposalId].eligibleRoot;
         commitmentInputs[2] = voterRoot;
         uint256 hasherIdx = 3;
-        for (uint256 i = 0;i < k1;i++) {
+        for (uint256 i ;i < k1;) {
             commitmentInputs[hasherIdx] = proposals[proposalId].options[option][i];
             hasherIdx++;
+            unchecked{
+                ++i;
+            }
         }
 
-        for (uint256 i = 0;i < 6;i++) {
-            for (uint256 j = 0;j < 2;j++) {
-                for (uint256 idx = 0;idx < k2;idx++) {
+        /// @dev Iterating the for loops 4 times  to prevent making 4 different loop functions 
+        uint z ;
+    while(z<=3){
+        for (uint256 i ;i < 6;) {
+            for (uint256 j ;j < 2;) {
+                for (uint256 idx ;idx < k2;) {
+                    if(hasherIdx <=79){
                     commitmentInputs[hasherIdx] = negalfa1xbeta2[i][j][idx];
                     hasherIdx++;
+                    }
+                    else if (hasherIdx <=151 ){
+                        commitmentInputs[hasherIdx] = gamma2[i][j][idx];
+                        hasherIdx++;
+                    }
+                    else if (hasherIdx <=223){
+                         commitmentInputs[hasherIdx] = delta2[i][j][idx];
+                         hasherIdx++;
+                    }
+                    else if (hasherIdx <=295){
+                        commitmentInputs[hasherIdx] = IC[i][j][idx];
+                        hasherIdx++;
+                    }
+                    unchecked{
+                        ++idx;
+                    }
+                }
+                unchecked{
+                    ++j;
                 }
             }
-        }
-
-        for (uint256 i = 0;i < 2;i++) {
-            for (uint256 j = 0;j < 2;j++) {
-                for (uint256 idx = 0;idx < k2;idx++) {
-                    commitmentInputs[hasherIdx] = gamma2[i][j][idx];
-                    hasherIdx++;
-                }
+            unchecked{
+                ++i;
             }
         }
-        
-        for (uint256 i = 0;i < 2;i++) {
-            for (uint256 j = 0;j < 2;j++) {
-                for (uint256 idx = 0;idx < k2;idx++) {
-                    commitmentInputs[hasherIdx] = delta2[i][j][idx];
-                    hasherIdx++;
-                }
-            }
-        }
-
-        for (uint256 i = 0;i < 2;i++) {
-            for (uint256 j = 0;j < 2;j++) {
-                for (uint256 idx = 0;idx < k2;idx++) {
-                    commitmentInputs[hasherIdx] = IC[i][j][idx];
-                    hasherIdx++;
-                }
-            }
-        }
+    }
 
         uint256 hash = uint256(sha256(abi.encode(commitmentInputs))) >> 6;
-        require(_input[0] == hash, "Bad public commitment");
+        if (_input[0] != hash){
+            revert BADCOMMITMENT();
+        }
 
         if (voteCount > proposals[proposalId].voteCount[option]) {
             proposals[proposalId].voteCount[option] = voteCount;
